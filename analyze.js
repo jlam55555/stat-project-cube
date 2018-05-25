dad38406af51241b4b32aebfc2707421fdc6b5f7
Jonathan Lam <jlam55555@gmail.com>
2018-05-24 23:16:06 -0400

    modified analyze file

--- analyze.js
+++ analyze.js
@@ -1,7 +1,22 @@
+// statistics code
+Array.prototype.mean = function() {
+  return this.reduce((a, v) => a + v) / this.length;
+};
+Array.prototype.std = function(mean) {
+  mean = mean || this.mean();
+  return Math.sqrt(this.reduce((a, v) => a + Math.pow(v - mean, 2), 0) / (this.length - 1));
+};
+
 // get data
 const data = require('./scrambles.js');
 
 // analyze each scrambler
 data.forEach(scrambler => {
-  process.stdout.write(`${scrambler.name}\nscrambles left: ${scrambler.scrambles.length}\nsolved (for sanity): ${scrambler.solved.length}\n`);
+  // calculate mean
+  let times = scrambler.solved.map(solve => solve.duration);
+  let mean = times.mean();
+  let std = times.std();
+
+  // print out data
+  process.stdout.write(`name:\t\t\t${scrambler.name}\nscrambles left:\t\t${scrambler.scrambles.length}\nsolved (for sanity):\t${scrambler.solved.length}\nmean:\t\t\t${mean}\nstandard deviation:\t${std}\ntimes:\t\t\t${times}\n------------------------\t\n`);
 });
